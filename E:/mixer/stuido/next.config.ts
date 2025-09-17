36059bba1c13a0c8aa6f3d96137833ebb2f8f3d0
Luis Uchubanda <ueservicesllc1@gmail.com>
2025-09-17 16:58:50 +0000

    Removing node_modules/ PS E:\mixer\stuido> git fetch origin; git reset -

---
+++ E:/mixer/stuido/next.config.ts
@@ -0,0 +1,41 @@
+import type {NextConfig} from 'next';
+require('dotenv').config();
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  distDir: 'out',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+  devIndicators: {
+    allowedDevOrigins: ['*'],
+  },
+};
+
+export default nextConfig;
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+git fetch origin; git reset --hard origin/main; git clean -dfx
 # Firebase Studio
 
 This is a NextJS starter in Firebase Studio.
